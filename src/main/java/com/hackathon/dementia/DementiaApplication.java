8df42de418cdb39011b40691c20a1341af7646e6
arya <arya@localhost>
2024-06-26 17:43:15 +0530

    First commit for maven project

---
+++ src/main/java/com/hackathon/dementia/DementiaApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.dementia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DementiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DementiaApplication.class, args);
+	}
+
+}
